1f05a5288728223753ecbc6f157a91e6341bada9
James Lamb <jameslamb13@gmail.com>
2020-09-30 17:10:33 -0400

    took out extra files and added some code to server.js. May need tweaking.

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var path = require("path");
 
 
 var app = express();
-
-var PORT = 5050;
+var PORT = process.env.PORT || 5050;
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,11 +12,9 @@ app.use(express.json());
 
 var reservedTables = [];
 
-var waitList = [];
 
-
-
-app.get("/", function(req, res) {
+// displaying HTML
+ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
   
@@ -28,26 +25,37 @@ app.get("/", function(req, res) {
   app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
   });
+
+  // Below is unfinished code
+  // Needs Work
+  app.get("/api/tables", (req, res) => {
+    reservedTables.slice(0, 5);
+  })
   
+  app.get("api/waitlist", (req,res) => {
+    reservedTables.slice(6);
+  })
 
-
-  app.post("/api/tables")
+ 
   
-
-
-// var data = {
-
-//     reservedTables = [],
-//     waitList = [],
-// }
-
-
-
-
-
+  // Below is unfinished code
+  // Needs Work
+  app.post("/api/tables", function(req, res) {
+    var newTable = req.body;
+    console.log(newTable)
+    
+    if (reservations.length < 5) {
+    reservedTables.push(newTable);
+    
+    res.json(newCharacter);
+  } else {
+    waitList.push(newTable)
+  };
+  
+});
 
 
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT http://localhost:" + PORT);
 });
